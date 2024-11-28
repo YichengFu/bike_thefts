229a23c7e5b1ae5bdaa6af34d4ee948a8d8946e0
Yicheng Fu <tommy.fu@mail.utoronto.ca>
2024-11-28 12:09:49 -0500

    model set up and reference updated

--- scripts/05-model_data.R
+++ scripts/05-model_data.R
@@ -1,37 +1,39 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Purpose: Built Models for logistic regression model to predict thefts in high-risk neighborhoods.
+# Author: Tommy Fu
+# Date: 27 November 2024
+# Contact: tommy.fu@mail.utoronto.ca
+# Pre-requisites: 
+# - All the necessary packages must be installed and loaded 'dplyr','tidyr','arrow','lubridate','tidyverse',testthat'.
+# - 02-clean_data.R must have been run
+# - 03-test_analysis_data must have been run and passed
+# - 04-exploratory_data_analysis.R must have been run
 
 #### Workspace setup ####
 library(tidyverse)
 library(rstanarm)
 
+
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+analysis_data <- read_parquet("data/02-analysis_data/analysis_data.parquet")
 
-### Model data ####
-first_model <-
-  stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
-  )
+# ensure outcome consistency by setting seed
+set.seed(432)
 
+# get the reduced dataset of only 1000 randomly selected data entries
+data_reduced <- 
+  analysis_data |> 
+  slice_sample(n = 1000)
 
-#### Save model ####
-saveRDS(
-  first_model,
-  file = "models/first_model.rds"
+#### Logistic Regression Model ####
+high_risk_model <- stan_glm(
+  formula = is_high_risk_neighborhood ~ BIKE_COST + PREMISES_TYPE + LOCATION_TYPE + OCC_HOUR,
+  data = data_reduced,
+  family = binomial(link = "logit"),
+  prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
+  prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
+  seed = 853
 )
 
-
+#### Save model ####
+saveRDS(high_risk_model,file = "models/first_model.rds")
